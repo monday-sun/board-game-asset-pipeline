b1ed10f600b1dcd7f8a65a75696964500476f83b
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-04-29 15:05:50 -0700

    MANUAL: simplify to async function

--- src/react-to-html/react-to-html.spec.tsx
+++ src/react-to-html/react-to-html.spec.tsx
@@ -1,23 +1,31 @@
 import '@testing-library/jest-dom';
-import { renderToString } from 'react-dom/server';
 import React from 'react';
-import { ReactToHtml } from './react-to-html';
+import { renderToString } from 'react-dom/server';
+import { convertToStaticHtml } from './react-to-html';
 
 jest.mock('react-dom/server', () => ({ renderToString: jest.fn() }));
+
+class TestComponent extends React.Component<{ name: string }> {
+  render() {
+    return <h1>Hello, {this.props.name}!</h1>;
+  }
+}
 
 describe('ReactToHtml', () => {
   it('should render component to static HTML', async () => {
     const componentPath = './path/to/component';
-    const props = { prop1: 'value1', prop2: 'value2' };
-    const mockComponent = () => <div>Mock Component</div>;
+    const props = { name: 'test_name' };
     const mockRenderToString = '<div>Mock Component</div>';
 
-    jest.mock('./path/to/component', () => mockComponent, { virtual: true });
+    jest.mock('./path/to/component', () => TestComponent, { virtual: true });
+
     (renderToString as jest.Mock).mockReturnValue(mockRenderToString);
 
-    const reactToHtml = new ReactToHtml();
-    const result = await reactToHtml.convertToStaticHtml(componentPath, props);
+    const result = await convertToStaticHtml(componentPath, props);
 
     expect(result).toBe(mockRenderToString);
+    expect(renderToString).toHaveBeenCalledWith(
+      <TestComponent name="test_name" />,
+    );
   });
 });
