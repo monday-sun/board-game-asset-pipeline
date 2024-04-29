3c3513fc62aa62591726b3897b5a071ab1a2db60
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-04-29 16:11:52 -0700

    MANUAL: renderToStaticMarkup seems like a better fit

--- src/react-to-html/react-to-html.tsx
+++ src/react-to-html/react-to-html.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 export async function convertToStaticHtml(
   componentPath: string,
   props: Record<string, any> = {},
 ): Promise<string> {
   const { default: Component } = await import(componentPath);
-  return renderToString(<Component {...props} />);
+  return renderToStaticMarkup(<Component {...props} />);
 }
