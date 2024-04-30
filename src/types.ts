b63cbb8cda582737a4959effa487c6c4990e91e4
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-04-30 16:23:20 -0700

    MANUAL: add wrapper class

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,7 @@ export type CardInfo = {
   backTemplate: string;
   [key: string]: string; // For unknown values
 };
+
+export interface LayoutRenderer {
+  toHTML(templatePath: string, data: Record<string, string>): Promise<string>;
+}
