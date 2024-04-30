0a1e65287b3a274af14e9e6d637dd7d99eec14d1
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-04-29 18:14:29 -0700

    FEATURE: make relative to working dir

--- src/react-to-html/react-to-html.tsx
+++ src/react-to-html/react-to-html.tsx
@@ -1,3 +1,4 @@
+import { cwd } from 'process';
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 
@@ -5,6 +6,6 @@ export async function convertToStaticHtml(
   componentPath: string,
   props: Record<string, any> = {},
 ): Promise<string> {
-  const { default: Component } = await import(componentPath);
+  const { default: Component } = await import(`${cwd()}/${componentPath}`);
   return renderToStaticMarkup(<Component {...props} />);
 }
