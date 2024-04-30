d48b4fb5a0013fc3eb225bbaa6e2998bb80341d4
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-04-29 18:09:48 -0700

    TEST: remove irrelevant test

--- src/react-card-gen.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('react-card-gen', () => {
-  it('should log "Hello from react-card-gen"', () => {
-    const consoleSpy = jest.spyOn(console, 'log');
-    require('./react-card-gen');
-    expect(consoleSpy).toHaveBeenCalledWith('Hello from react-card-gen');
-    consoleSpy.mockRestore();
-  });
-});
