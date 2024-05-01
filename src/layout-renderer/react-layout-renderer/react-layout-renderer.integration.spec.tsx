48dd23dd6cdc3e1704e8353459c9b54f30e3076e
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-04-30 17:49:01 -0700

    TOOL: rename files

---
+++ src/layout-renderer/react-layout-renderer/react-layout-renderer.integration.spec.tsx
@@ -0,0 +1,13 @@
+import { ReactLayoutRenderer } from './react-layout-renderer';
+
+describe('convertToStaticHtml', () => {
+  it('should render a React component to static HTML with properties', async () => {
+    const html = await new ReactLayoutRenderer().toHTML(
+      'src/layout-renderer/react-layout-renderer/test-component',
+      {
+        message: 'Hello, world!',
+      },
+    );
+    expect(html).toBe('<div>Hello, world!</div>');
+  });
+});
--- src/react-card-gen.ts
+++ src/react-card-gen.ts
@@ -8,7 +8,7 @@ const pathToCSV = './src/test-cards.csv';
 let allPromises: Promise<void>[] = [];
 
 parseCsvToCardInfo(pathToCSV).then(async (cardInfos) => {
-  import('./layout-renderer/react-to-html/react-to-html').then(
+  import('./layout-renderer/react-layout-renderer/react-layout-renderer').then(
     ({ layoutRenderer }) => {
       allPromises = cardInfos.map((cardInfo) => {
         return Card.from(cardInfo, layoutRenderer)
