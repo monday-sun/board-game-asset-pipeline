a471be1e9e841de2169cee6b24dbeef0744dcb1d
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-05-10 11:47:33 -0700

    MANUAL: reorder

--- src/layout/react/render-worker.ts
+++ src/layout/react/render-worker.ts
@@ -1,15 +1,16 @@
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
+
+export function render(templatePath: string, data: any) {
+  return import(`${templatePath}`).then(({ default: Component }) => {
+    return renderToStaticMarkup(React.createElement(Component, data));
+  });
+}
+
 if (require.main === module) {
   const [templatePath, dataString] = process.argv.slice(2);
   const data = JSON.parse(dataString);
   render(templatePath, data).then((html) => {
     process.stdout.write(html);
-  });
-}
-
-export function render(templatePath: string, data: any) {
-  return import(`${templatePath}`).then(({ default: Component }) => {
-    return renderToStaticMarkup(React.createElement(Component, data));
   });
 }
