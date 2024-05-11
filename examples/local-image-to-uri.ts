0000dc6aae10454c85d42f56594a48c141fa703f
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-05-11 14:33:11 -0700

    TOOL: delete examples

--- examples/local-image-to-uri.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-export function localImageToUri(imageUrl: string) {
-  const image = fs.readFileSync(imageUrl);
-  const base64Image = Buffer.from(image).toString('base64');
-  const dataURI = 'data:image/jpeg;base64,' + base64Image;
-  return dataURI;
-}
