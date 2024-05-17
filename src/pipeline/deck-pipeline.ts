5e2e086d0dbb44d21bb2edce0fa81d80f7b0eacb
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-05-17 16:15:38 -0700

    RISKY: convert to combineLatest

--- src/pipeline/deck-pipeline.ts
+++ src/pipeline/deck-pipeline.ts
@@ -1,54 +1,31 @@
-import { Subscription, lastValueFrom } from 'rxjs';
-import { Cards } from '../cards';
+import { Subscription, combineLatest, lastValueFrom, map } from 'rxjs';
+import { Cards, CardsFactory } from '../cards';
 import { Deck } from '../config';
 import { File } from '../file/file';
-import { Layout } from '../layout';
+import { Layout, LayoutFactory } from '../layout';
 import { Output } from '../output';
-import { Templates } from '../templates';
+import { Templates, TemplatesFactory } from '../templates';
 import { Arguements } from '../types';
 
 export function createDeckPipeline(args: Arguements, deckConfig: Deck) {
   const deckSubscriptions: Subscription[] = [];
-  Promise.all([
-    lastValueFrom(Cards.findFactory(args, deckConfig)),
-    lastValueFrom(Templates.findFactory(args, deckConfig)),
-    lastValueFrom(Layout.findFactory(args, deckConfig)),
-  ])
-    .then(([cardsFactory, templatesFactory, layoutFactory]) => {
-      const cards$ = cardsFactory(args, deckConfig);
-      deckSubscriptions.push(
-        cards$.subscribe(() =>
-          console.log('Loaded cards from', deckConfig.list),
-        ),
-      );
-
-      const templates$ = templatesFactory(
+  combineLatest([
+    // find needed factories in parallel
+    Cards.findFactory(args, deckConfig),
+    Templates.findFactory(args, deckConfig),
+    Layout.findFactory(args, deckConfig),
+  ]).pipe(
+    map(([cardsFactory, templatesFactory, layoutFactory]) => {
+      return createLayoutPipeline(
         args,
         deckConfig,
-        cards$,
-        File.factory,
+        cardsFactory,
+        templatesFactory,
+        layoutFactory,
+        deckSubscriptions,
       );
-      deckSubscriptions.push(
-        templates$.subscribe(({ templatePaths }) =>
-          console.log('Requested layout for template', templatePaths.filePath),
-        ),
-      );
-
-      const layout$ = layoutFactory(args, deckConfig, templates$);
-      deckSubscriptions.push(
-        layout$.subscribe(({ templatePaths, card }) =>
-          console.log(
-            'Generated layout for card',
-            card.name,
-            'with template',
-            templatePaths.filePath,
-          ),
-        ),
-      );
-
-      return layout$;
-    })
-    .then((layout) => {
+    }),
+    map((layout) => {
       deckConfig.output.forEach((outputConfig) =>
         lastValueFrom(Output.findFactory(outputConfig)).then((factory) => {
           const generated$ = factory(args, outputConfig, layout);
@@ -62,6 +39,42 @@ export function createDeckPipeline(args: Arguements, deckConfig: Deck) {
           return generated$;
         }),
       );
-    });
+    }),
+  );
   return deckSubscriptions;
+}
+
+function createLayoutPipeline(
+  args: Arguements,
+  deckConfig: Deck,
+  cardsFactory: CardsFactory,
+  templatesFactory: TemplatesFactory,
+  layoutFactory: LayoutFactory,
+  deckSubscriptions: Subscription[],
+) {
+  const cards$ = cardsFactory(args, deckConfig);
+  deckSubscriptions.push(
+    cards$.subscribe(() => console.log('Loaded cards from', deckConfig.list)),
+  );
+
+  const templates$ = templatesFactory(args, deckConfig, cards$, File.factory);
+  deckSubscriptions.push(
+    templates$.subscribe(({ templatePaths }) =>
+      console.log('Requested layout for template', templatePaths.filePath),
+    ),
+  );
+
+  const layout$ = layoutFactory(args, deckConfig, templates$);
+  deckSubscriptions.push(
+    layout$.subscribe(({ templatePaths, card }) =>
+      console.log(
+        'Generated layout for card',
+        card.name,
+        'with template',
+        templatePaths.filePath,
+      ),
+    ),
+  );
+
+  return layout$;
 }
