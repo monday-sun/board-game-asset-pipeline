2e44179a4a85b8ecd0a871d236f8484d1667a876
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-05-22 13:12:19 -0700

    MANUAL: move templates pipeline

--- src/decks/deck-pipeline.ts
+++ src/decks/deck-pipeline.ts
@@ -1,38 +1,10 @@
 import { Observable, merge, mergeMap, shareReplay, tap } from 'rxjs';
 import { Deck, OutputConfig } from '.';
-import { Card, Cards } from '../cards';
-import { File } from '../file/file';
-import { FileContent } from '../file/file-content';
+import { Cards } from '../cards';
 import { Layout, LayoutResult } from '../layout';
 import { Output, OutputFilename } from '../output';
 import { NeedsLayout, Templates } from '../templates';
 import { Arguments } from '../types';
-
-
-
-function templatesPipeline(
-  args: Arguments,
-  deck: Deck,
-  cards$: Observable<Card[]>,
-  endWatch$: Observable<boolean> | undefined,
-) {
-  return Templates.findFactory(args, deck).pipe(
-    mergeMap((templatesFactory) =>
-      templatesFactory(args, deck, cards$, (args, path) =>
-        File.factory(args, path, endWatch$),
-      ),
-    ),
-    tap(({ templatePaths, card }) =>
-      console.log(
-        'Requested layout for card:',
-        card.name,
-        'side:',
-        card.frontTemplate === templatePaths.filePath ? 'front' : 'back',
-      ),
-    ),
-    shareReplay(),
-  );
-}
 
 function layoutPipeline(
   args: Arguments,
@@ -70,7 +42,7 @@ export function deckPipeline(
   endWatch$?: Observable<boolean>,
 ) {
   const cards$ = Cards.pipeline(args, deck, endWatch$);
-  const templates$ = templatesPipeline(args, deck, cards$, endWatch$);
+  const templates$ = Templates.pipeline(args, deck, cards$, endWatch$);
   const layout$ = layoutPipeline(args, deck, templates$);
 
   const outputPipelines: Observable<OutputFilename[]>[] = [];
