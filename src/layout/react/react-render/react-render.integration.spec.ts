6981f9d8580d426f6f977660b970a9bc7421e0af
Monday Romelfanger <monday.romelfanger@gmail.com>
2024-05-24 11:22:36 -0700

    TEST: improve test runner

--- src/layout/react/react-render/react-render.integration.spec.ts
+++ src/layout/react/react-render/react-render.integration.spec.ts
@@ -4,55 +4,75 @@ import * as util from 'util';
 const exec = util.promisify(execFile);
 
 describe('react-render', () => {
-  it.each([
-    {
-      file: './test/test-component',
-      data: { message: 'Hello!' },
-    },
-    {
-      file: './test/test-component',
-      data: { message: 'Goodbye!' },
-    },
-    {
-      file: './does-not-exist-component',
-      data: {},
-    },
-  ])('should load %p', ({ file, data }, done: jest.DoneCallback) => {
-    const fakeSubject = exec('ts-node', [
-      './src/layout/react/react-render/test/fake-react-render',
-      file,
-      JSON.stringify(data),
-    ]);
-    const testSubject = exec('ts-node', [
-      './src/layout/react/react-render/react-render',
-      file,
-      JSON.stringify(data),
-    ]);
+  it.each`
+    case
+    ${{
+  name: 'Hello',
+  file: './test/test-component',
+  data: { message: 'Hello!' },
+}}
+    ${{
+  name: 'Goodbye',
+  file: './test/test-component',
+  data: { message: 'Goodbye!' },
+}}
+    ${{
+  name: 'Error',
+  file: './does-not-exist-component',
+  data: {},
+}}
+  `(
+    'should load $case.name',
+    ({ case: { file, data } }, done: jest.DoneCallback) => {
+      const fakeSubject = exec('ts-node', [
+        './src/layout/react/react-render/test/fake-react-render',
+        file,
+        JSON.stringify(data),
+      ]);
+      const testSubject = exec('ts-node', [
+        './src/layout/react/react-render/react-render',
+        file,
+        JSON.stringify(data),
+      ]);
 
-    fakeSubject
-      .then((expectedResult) => {
-        testSubject
-          .then((result) => {
-            expect(result.stdout).toEqual(expectedResult.stdout);
-            expect(result.stderr).toEqual(expectedResult.stderr);
-            done();
-          })
-          .catch((error) => {
-            // Error should not be thrown if it wasn't expected
-            expect(error).toBeFalsy();
-            done();
-          });
-      })
-      .catch((expectedError: Error) => {
-        testSubject.catch((error: Error) => {
-          const afterError = expectedError.message.split('Error: ').pop();
-          const errorMessage = afterError?.split(`\n`)[0];
-          expect(errorMessage).toBeTruthy();
-          expect(error.message).toEqual(
-            expect.stringContaining(errorMessage as string),
-          );
-          done();
+      fakeSubject
+        .then((expectedResult) => {
+          testSubject
+            .then((result) => {
+              console.log(result);
+              console.log(expectedResult);
+              expect(result.stdout).toEqual(expectedResult.stdout);
+              expect(result.stderr).toEqual(expectedResult.stderr);
+              done();
+            })
+            .catch((error) => {
+              // Error should not be thrown if it wasn't expected
+              expect(error).toBeFalsy();
+              console.log(error);
+              done();
+            });
+        })
+        .catch((expectedError: Error) => {
+          testSubject
+            .then((result) => {
+              // should not have result if error was expected
+              expect(result).toBeFalsy();
+              console.log(result);
+              done();
+            })
+            .catch((error: Error) => {
+              const afterError = expectedError.message.split('Error: ').pop();
+              const errorMessage = afterError?.split(`\n`)[0];
+              expect(errorMessage).toBeTruthy();
+              expect(error.message).toEqual(
+                expect.stringContaining(errorMessage as string),
+              );
+              console.log('Error', error);
+              console.log('Expected', expectedError);
+
+              done();
+            });
         });
-      });
-  });
+    },
+  );
 });
